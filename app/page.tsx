6c5c643e0959ea6249aaea01725c22933e53aa74
NSOLAf1 <nsolaf1@gmail.com>
2025-11-10 15:46:23 +0600

    home; css img

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from "next/image";
+// import styles from ''; // Import the CSS module
+import './globals.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-      <button>Click ME</button>
+    <div className="container">
+    <div className="header">
+      <h1>Header</h1>
     </div>
-  );
+
+    <div className="body">
+      <h1>Beautify</h1>
+      <img
+        src="https://www.w3schools.com/w3css/img_car.jpg"
+        alt="Car"
+        className="image"
+      />
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim culpa molestias totam officiis nihil velit architecto id iusto ipsa quasi aut iure vero minus, sed molestiae sint eum hic accusamus!</p>
+    </div>
+
+    <div className="footer">
+      <h5>Footer</h5>
+    </div>
+  </div>
+    );
 }
